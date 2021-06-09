b5c79d1f608a8d67323fe4f43ccad455fd560e51
Aaron Kurowski <aaron12342@att.net>
2021-06-09 15:40:23 -0500

    DefineContestants and opening statement functions added to game class. Game decides the contestants at the instantiation of the game.

--- Game.cs
+++ Game.cs
@@ -9,21 +9,62 @@ namespace RPSLS
     class Game
     {
         // member variables
+        int numberOfRounds;
 
         // constructor
+        public Game()
+        {
+            DefineContestants();
+        }
 
         // member methods
         public void RunGame()
         {
-            Player playerOne = new Player();
-            Ai npc = new Ai();
-            
-            playerOne.SelectName();
-            playerOne.PickGesture();
-            npc.SelectName();
-            npc.PickGesture();
+            // opening statement
+            // define contestants
+            // battle
+            // declare winner
+
+            OpeningStatements();
+
 
             Console.ReadLine();
         }
+
+        public void OpeningStatements()
+        {
+            //Console.WriteLine("\nWelcome to Rock, Paper, Scissors, Lizard, Spock");
+            Console.WriteLine("\nWelcome to Rock, Paper, Scissors, Lizard, Spock!" +
+              "\n\nHere are the rules:" +
+              "\n\nRock crushes Scissors" +
+              "\nScissors cuts Paper" +
+              "\nPaper covers Rock" +
+              "\nRock crushes Lizard" +
+              "\nLizard poisons Spock" +
+              "\nSpock smashes Scissors" +
+              "\nScissors decapitates Lizard" +
+              "\nLizard eats paper" +
+              "\nPaper disproves Spock" +
+              "\nSpock vaporizes Rock");
+        }
+
+        public void DefineContestants()
+        {
+            Console.WriteLine("Press 1 to play against a Human or type anything else to play against an AI");
+            string opponent = Console.ReadLine();
+
+            if(opponent == "1")
+            {
+                Player playerOne = new Player();
+                Player playerTwo = new Player();
+            }
+            else
+            {
+                Player playerOne = new Player();
+                Ai npc = new Ai();
+            }
+        }
+
+
     }
 }
